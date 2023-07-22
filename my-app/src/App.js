4b0ec558dcf5615a121fa80ab947812444c45971
karungieliza@gmail.com <Karungi Elizabeth>
2023-07-22 12:01:58 +0300

    first file

---
+++ my-app/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function MyButton(){
+  return(
+      <button>Save</button>
+  );
+}
+export default function MyApp(){
+  return(
+      <div>
+          <h1>
+              hi there,
+              
+          </h1>
+          <p>this is my first button i have created using react.</p>
+          <MyButton/>
+      </div>
+  );
+}
