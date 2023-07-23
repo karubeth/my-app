ea67cc2006af6dde04a1a040ecfefc836681d060
karungieliza@gmail.com <Karungi Elizabeth>
2023-07-23 22:46:31 +0300

    chaged from a mere button to a tic- tac-toe game

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,19 +1,42 @@
-import React from 'react';
+import {useState} from 'react';
 
-function MyButton(){
-  return(
-      <button>Save</button>
-  );
+
+  function Square({value},{onSquareClick}){
+    //const[value , setValue] = useState(Array(9).fill(null));
+
+  /*function handleClick(){
+    setValue('x');
+  }*/
+  return <button className="square" onClick = {onSquareClick}>{value}</button>
+    
+   
 }
-export default function MyApp(){
-  return(
-      <div>
-          <h1>
-              hi there,
-              
-          </h1>
-          <p>this is my first button i have created using react.</p>
-          <MyButton/>
-      </div>
-  );
-}
+export default function Board(){//main function
+  const[squares , setSquares] = useState(Array(9).fill(null));
+  function handleClick(){
+    const nextSquares=squares.slice();
+    nextSquares[0] = "x";
+    setSquares(nextSquares);
+  }
+  return (//the different square boxes returned by the function 
+   <>
+    <div>
+     <Square value = {squares[0]} onSquareClick = {handleClick} />
+     <Square value = {squares[1]} />
+     <Square value = {squares[2]} />
+    </div>
+
+    <div>
+     <Square value = {squares[3]} />
+     <Square value = {squares[4]} />
+     <Square value = {squares[5]} /> 
+    </div>
+
+    <div>
+     <Square value = {squares[6]} />
+     <Square value = {squares[7]} />
+     <Square value = {squares[8]} />
+    </div>
+  </>
+);
+  }
